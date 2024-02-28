cef5d8b26cda0dc024c98e113dd8b8bc6b036ecd
Fernando Minardi <minardifer@gmail.com>
2024-02-28 09:39:05 -0300

    Remove unused actions

--- app/Http/Controllers/Api/V1/CollaboratorController.php
+++ app/Http/Controllers/Api/V1/CollaboratorController.php
@@ -28,14 +28,6 @@ class CollaboratorController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreCollaboratorRequest $request)
@@ -56,14 +48,6 @@ class CollaboratorController extends Controller
         }
 
         return new CollaboratorResource($query->first());
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Collaborator $collaborator)
-    {
-        //
     }
 
     /**
